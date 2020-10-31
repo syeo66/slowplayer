12499e910134cb7c28ce2c13b6b7f23ed0c40dd3
Red Ochsenbein <red@redochsenbein.ch>
2020-10-31 21:54:41 +0100

    Change dithering

--- play.sh
+++ play.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-VIDEO=/home/pi/Videos/BigBucksBunny.mp4
+VIDEO=/home/pi/Videos/Interstellar.mp4
+#VIDEO=/home/pi/Videos/Arrival.mp4
 
 MAXCOUNT=$(ffprobe -show_format $VIDEO 2>/dev/null | grep -F duration | cut -d= -f2 | xargs printf %.0f)
 COUNT=$(cat /home/pi/position)
@@ -10,7 +11,8 @@ echo $COUNT/$MAXCOUNT
 if [ $COUNT -lt $MAXCOUNT ]; then
 	sudo IT8951/IT8951 0 0 0.bmp
 	ffmpeg -ss $COUNT -y -i $VIDEO -vf "scale=h=600:w=800:force_original_aspect_ratio=increase,crop=w=800:h=600" -vframes 1 -s 800x600 1.bmp
-	convert 1.bmp -colorspace gray -ordered-dither o8x8 0.bmp	
+	#convert 1.bmp -colorspace gray -ordered-dither o8x8 0.bmp	
+	convert 1.bmp -colorspace gray -dither FloydSteinberg 0.bmp	
 	let "COUNT+=1"
 	echo $COUNT > /home/pi/position	
 else
