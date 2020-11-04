a2438f3c1bd4be885fbc68614524a8d8de137f2d
Red Ochsenbein <red.ochsenbein@mindfire.global>
2020-11-04 06:45:26 +0100

    Add path variable for IT8951

--- play.sh
+++ play.sh
@@ -3,13 +3,15 @@
 VIDEO=/home/pi/Videos/Interstellar.mp4
 #VIDEO=/home/pi/Videos/Arrival.mp4
 
+IT8951=/home/pi/slowplayer/IT8951/IT8951
+
 MAXCOUNT=$(ffprobe -show_format $VIDEO 2>/dev/null | grep -F duration | cut -d= -f2 | xargs printf %.0f)
 COUNT=$(cat /home/pi/position)
 
 echo $COUNT/$MAXCOUNT
 
 if [ $COUNT -lt $MAXCOUNT ]; then
-	sudo IT8951/IT8951 0 0 0.bmp
+	sudo $IT8951 0 0 0.bmp
 	ffmpeg -ss $COUNT -y -i $VIDEO -vf "scale=h=600:w=800:force_original_aspect_ratio=increase,crop=w=800:h=600" -vframes 1 -s 800x600 1.bmp
 	convert 1.bmp -level 0%,100%,1.7 -colorspace gray -ordered-dither o8x8 0.bmp	
 	#convert 1.bmp -level 0%,100%,1.7 -colorspace gray -dither FloydSteinberg 0.bmp
