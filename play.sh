da122b2c35bb8489a0456fdfa4b367afaf49aabe
Red Ochsenbein <red.ochsenbein@mindfire.global>
2021-04-22 10:34:14 +0200

    Add code to automatically rotate videos

--- play.sh
+++ play.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-VIDEO=/home/pi/Videos/Interstellar.mp4
-#VIDEO=/home/pi/Videos/Arrival.mp4
+VIDEO_PATH=/home/pi/Videos/
 
-IT8951=/home/pi/slowplayer/IT8951/IT8951
-
+CURRENT=$(cat /home/pi/video)
+VIDEO=$VIDEO_PATH$CURRENT
 MAXCOUNT=$(ffprobe -show_format $VIDEO 2>/dev/null | grep -F duration | cut -d= -f2 | xargs printf %.0f)
 COUNT=$(cat /home/pi/position)
 
-echo $COUNT/$MAXCOUNT
+echo "$CURRENT: $COUNT/$MAXCOUNT"
+
+scp /home/pi/0.bmp red@srv.raven.ch:/var/www/redochsenbein.ch/web/
 
 if [ $COUNT -lt $MAXCOUNT ]; then
-	sudo $IT8951 0 0 0.bmp
+	sudo IT8951/IT8951 0 0 0.bmp
 	ffmpeg -ss $COUNT -y -i $VIDEO -vf "scale=h=600:w=800:force_original_aspect_ratio=increase,crop=w=800:h=600" -vframes 1 -s 800x600 1.bmp
-	convert 1.bmp -level 0%,100%,1.7 -colorspace gray -ordered-dither o8x8 0.bmp	
-	#convert 1.bmp -level 0%,100%,1.7 -colorspace gray -dither FloydSteinberg 0.bmp
-        #convert 1.bmp \
-	#	\( -clone 0 -channel RG -separate +channel -evaluate-sequence mean \) \
-	#	\( -clone 0 -channel GB -separate +channel -evaluate-sequence mean \) \
-	#	-delete 0 -evaluate-sequence mean -dither FloydSteinberg 0.bmp	
+	convert 1.bmp -normalize -level 0%,100%,1.5 -colorspace gray -ordered-dither o8x8 0.bmp	
 	let "COUNT+=1"
 	echo $COUNT > /home/pi/position	
 else
 	echo 1 > /home/pi/position
+	for f in $(ls $VIDEO_PATH && ls $VIDEO_PATH);
+	do
+		if [ "$FOUND" == "1" ]; then
+			echo "Next video: $f"
+			echo $f > /home/pi/video
+			FOUND=0
+			exit 0
+		fi
+		if [ "$CURRENT" == "$f" ]; then
+			FOUND=1
+		fi
+	done;
 fi
